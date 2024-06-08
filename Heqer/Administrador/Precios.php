9244af220736d8f23e647c760d0241aea1b122e0
EmmanuelAcoltziBautista <140022194+EmmanuelAcoltziBautista@users.noreply.github.com>
2024-06-07 19:52:54 -0600

    Add files via upload

---
+++ Heqer/Administrador/Precios.php
@@ -0,0 +1,89 @@
+<?php
+require_once '../Database/Basedatos.php';
+$conexionbd=mysqli_connect(server,user,password,database,port);
+$query="SELECT * FROM `PRECIOS`;";
+$resultado=mysqli_query($conexionbd,$query);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="../images/usuario.png">
+    <link rel="stylesheet" href="../estilosCss/alert.css">
+    <script src="../js/alert.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heqer</title>
+<link rel="stylesheet" href="../estilosCss/estilos1.css">
+<style type="text/css">
+select{
+font-size:20px;
+margin:3px;
+padding:3px;
+
+}
+
+table,th,td{
+color:rgb(255,255,255);
+
+}
+</style>
+</head>
+<body>
+    <div class="gradiente">
+<a href="./">Regresar</a>
+<a href="../">Cerrar sesión</a>
+<center>
+<h1>Precios</h1>
+<table border="1">
+<tr>
+<th>Estudio</th>
+<th>Precio</th>
+</tr>
+<ul>
+<?php
+while($r1=mysqli_fetch_assoc($resultado)){
+echo"<tr><td>".$r1["ESTUDIO"]."</td><td>".$r1["PRECIO"]."</td>";
+}
+?>
+
+<br/>
+
+<form method="post">
+<select id="Estudio" name="Estudio">
+<?php
+$cone=mysqli_connect(server,user,password,database,port);
+$RESU=mysqli_query($cone,$query);
+while($r2=mysqli_fetch_assoc($RESU)){
+echo"<option value=".$r2["ESTUDIO"].">".$r2["ESTUDIO"]."</option>";
+}
+?>
+</select>
+<label for="Precio"></label>
+<input type="text" id="Precio" name="Precio" placeholder="Precio">
+<input type="submit" id="Enviar" name="Enviar" value="Actualizar" class="boton">
+
+</form>
+<?php
+$conexion11=mysqli_connect(server,user,password,database,port);
+if(ISSET($_POST["Enviar"])){
+$PRECIO=$_POST["Precio"];
+$ESTUDIO=$_POST["Estudio"];
+$query2="UPDATE `PRECIOS` SET PRECIO=".$PRECIO." WHERE ESTUDIO='".$ESTUDIO."';";
+//echo $query2;
+$SALIDA=mysqli_query($conexion11,$query2);
+if($SALIDA>0){
+    echo"<script>
+    swal.fire({
+        position:'center',
+        title:'Buen trabajo',
+        icon:'success',
+    });
+    </script>";
+}
+}
+?>
+</center>
+</div>
+</body>
+</html>
---
+++ Heqer/Recepcionista/EOrina.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heqer</title>
+    <link rel="icon" href="../images/usuario.png">
+<link rel="stylesheet" href="../estilosCss/estilos1.css">
+<style type="text/css">
+.BoxSalida{
+background:rgb(255,255,255);
+border:3px solid rgb(0,0,0);
+font-size:14px;
+font-family:Arial;
+font-weight:normal;
+width:50%;
+}
+.AlineacionD{
+text-align:right;
+margin:2px;
+padding:2px;
+}
+.AlineacionI{
+text-align:left;
+margin:2px;
+padding:2px;
+}
+
+
+
+</style>
+<script src="../js/Imprimir.js">
+</script>
+
+</head>
+<body>
+    <div class="gradiente">
+<a href="./">Regresar</a>
+<a href="../">Cerrar sesión</a>
+<center>
+<h1>Resultados EGO</h1>
+<form method="post">
+<label for="CLAVE">Clave</label>
+<input type="text" id="CLAVE" name="CLAVE" placeholder="Clave:" required>
+<input type="submit" id="enviar" name="enviar" value="Mostrar" class="boton">
+<form>
+<?php
+$SALIDAout="";
+$CLAVE="";
+require_once '../Database/Basedatos.php';
+if(ISSET($_POST["enviar"])){
+$conexionbd=mysqli_connect(server,user,password,database,port);
+$CLAVE=$_POST["CLAVE"];
+$query="SELECT * FROM `ORINA` WHERE CLAVE='".$CLAVE."';";
+$RESULTADO=mysqli_query($conexionbd,$query);
+
+$query2="SELECT * FROM `ALTAS` WHERE CLAVE='".$CLAVE."';";
+$conexionbd2=mysqli_connect(server,user,password,database,port);
+$Resultado2=mysqli_query($conexionbd2,$query2);
+
+$REGISTRO=mysqli_fetch_assoc($RESULTADO);
+$DATOS=mysqli_fetch_assoc($Resultado2);
+if($REGISTRO && $DATOS){
+$FECHA=$DATOS["FECHA"];
+$HORA=$DATOS["HORA"];
+$NOMBRE=$DATOS["NOMBRE"];
+$EDAD=$DATOS["EDAD"];
+$SEXO=$DATOS["SEXO"];
+
+$COLOR=$REGISTRO["COLOR"];
+$EMBARAZO=$REGISTRO["EMBARAZO"];
+$PH=$REGISTRO["PH"];
+$COMENTARIOS=$REGISTRO["COMENTARIOS"];
+
+date_default_timezone_set('America/Mexico_City');
+
+$SALIDAout="
+<br/>
+<p class='AlineacionD'>
+Fecha de impresion:<b>.".date('d-m-Y')."</b>
+<br/>
+Hora de impresion:<b>".date('H:i:s')."</b>
+<br/></br>
+Fecha de muestras: <b>".$FECHA."</b><br/>
+Hora de muestras:<b>".$HORA."</b>
+</p>
+
+<p class='AlineacionI'>
+<br/>
+
+Nombre: <b>".$NOMBRE."</b> Edad: <b>".$EDAD."</b> Sexo biologico: <b>".$SEXO."</b>
+<br/>
+<br/>
+Color: <b>".$COLOR."</b>
+<br/>
+
+Embarazo: <b>".$EMBARAZO."</b>
+<br/>
+PH: <b>".$PH."</b>
+<br/>
+<br/>
+Comentarios:<br/>
+<b>".$COMENTARIOS."
+</b>
+</p>
+
+";
+
+}
+}
+?>
+<div class="BoxSalida">
+<?php echo $SALIDAout; ?>
+
+</div>
+<?php
+$conexion=mysqli_connect(server,user,password,database,port);
+$q2='INSERT INTO `IMPRIMIR`(`ID`,`CLAVE`,`ESTUDIO`,`TEXTO`) VALUES("0","'.$CLAVE.'","3","'.$SALIDAout.'");';
+$resultado5=mysqli_query($conexion,$q2);
+?>
+<a href="Imprimir.php?CLAVE=<?php echo $CLAVE; ?>&ESTUDIO=3" target="_blank" class="boton">Imprimir</a>
+
+</div>
+</body>
+</html>
